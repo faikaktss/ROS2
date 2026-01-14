86c545e86fe4a46a69b0e65658169f17fb07c549
𝐅𝐚𝐢𝐤 𝐀𝐤𝐭𝐚𝐬 <148656104+faikaktss@users.noreply.github.com>
2026-01-14 16:29:36 +0300

    Subscriber kısmı yazıldı artık yayıncının bir dinleyicisi var

---
+++ my_pk_pkg/my_pk_pkg/smartphone.py
@@ -0,0 +1,16 @@
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import String
+
+class SmartphoneNode(Node):
+    def __init__(self):
+        super().__init__("smartphone")
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SmartphoneNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
